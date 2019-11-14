bc7591add0408e39a24a611dda59c6136a779930
Maryna Glukhodid <glukhodid.m@gmail.com>
2019-11-14 21:15:35 +0100

    Create routes and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "Triple sec")
+Ingredient.create(name: "Amaretto")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Cherry brandy")
